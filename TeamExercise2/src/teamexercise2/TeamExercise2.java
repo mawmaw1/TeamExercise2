c1a5848a661a82fe3961f172a37162df02a62345
ChristofferMikkelsen <mikkelsen3@gmail.com>
2015-09-22 11:50:34 +0200

    test 1

--- TeamExercise2/src/teamexercise2/TeamExercise2.java
+++ TeamExercise2/src/teamexercise2/TeamExercise2.java
@@ -25,16 +25,13 @@ public class TeamExercise2 {
     }
     
     public void cock(){
-<<<<<<< HEAD
 
             int test = 0;
 
             
 
             System.out.println(test);
-=======
-            
->>>>>>> parent of a02430a... test 1
+
         }
     
 }
